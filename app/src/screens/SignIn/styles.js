059e0f7c2767a67734cbaac6b6c6e49741ca28cd
ernanijose <ernanijose@gmail.com>
2020-12-01 20:51:28 -0300

    Iniciando a tela de login

--- app/src/screens/SignIn/styles.js
+++ app/src/screens/SignIn/styles.js
@@ -2,4 +2,30 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+    background-color: #63C2D1;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const InputArea = styled.View``;
+
+export const CustomButton = styled.TouchableOpacity``;
+
+export const CustomButtonText = styled.Text``;
+
+export const SignMessageButton = styled.TouchableOpacity``;
+
+export const SignMessageButtonText = styled.Text``;
+
+export const SignMessageButtonTextBold = styled.Text``;
+
+/*
+InputArea,
+    CustomButton,
+    CustomButtonText,
+    SignMessageButton,
+    SignMessageButtonText,
+    SignMessageButtonTextBold
+*/
